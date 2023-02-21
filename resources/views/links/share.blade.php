d07d05e672e4626c24f6b7a22cffe492a4014945
Janta Roy Antor <crewshedg@gmail.com>
2023-02-21 17:20:03 +0600

    Modified extend layout to remove the nav bar in links page

--- resources/views/links/share.blade.php
+++ resources/views/links/share.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.links')
 
 @section('content')
-<div class="container">
+<div class="container py-5">
     <div class="row">
         <div class="col-12 col-md-6 offset-md-3">
             <div class="link">
